603214e42ff00f3b0235e02a3513292ff1cb038d
DiegoCopat <diegocopat@gmail.com>
2021-05-18 17:36:06 +0200

    impostata grafica html + css

---
+++ js/script.js
@@ -0,0 +1,21 @@
+var app = new Vue(
+    {
+        el: "#root",
+        data: {
+            srcImage: [
+                "https://www.animalieanimali.eu/wp-content/uploads/2016/01/animali-selvatici.jpg",
+                "https://www.ildigitale.it/wwf-persi-due-terzi-di-fauna-selvatica/",
+                "https://www.ilmiocaneleggenda.it/cani-che-sembrano-degli-animali-selvatici/",
+                "https://www.laleggepertutti.it/163762_animali-selvatici-in-strada-in-caso-di-incidente-chi-paga",
+                "https://www.fulviovilla.com/tutorial/fotografare-gli-animali/",
+                "https://www.infocilento.it/2019/01/12/nel-parco-limmissione-di-esemplari-di-lepre/",
+                "https://www.informazioneambiente.it/animali-erbivori/",
+                "https://pixnio.com/it/animali/uccelli/anatra/animale-germano-reale-fauna-selvatica-piuma-uccello-becco-natura-anatra-selvaggio",
+                "http://www.pixolo.it/2012/12/nuova-selezione-30-fantastiche-immagini-animali-alta-risoluzione/",
+                "https://www.wonews.it/post/immagini-animali-selvatici-julian-rad"
+            ]
+        },
+        methods: ""
+    }
+    
+)
